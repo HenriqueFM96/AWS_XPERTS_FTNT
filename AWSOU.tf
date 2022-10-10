4b1882de0942703cba25b43beea9bc3fd83f0af6
Henrique Moises <hmoises@fortinet-us.com>
2022-10-10 18:59:28 -0300

    OUs creation

--- AWSOU.tf
+++ AWSOU.tf
@@ -1,22 +1,14 @@
 # AWS aws_organizations
 data "aws_organizations_organization" "xperts" {}
 
-# DEV - OU
-resource "aws_organizations_organizational_unit" "dev" {
-  name      = "DEV"
+# STUDENTS - OU
+resource "aws_organizations_organizational_unit" "XPERTS-students" {
+  name      = "STUDENTS"
   parent_id = data.aws_organizations_organization.xperts.roots.0.id
 }
 
-# TEST - OU
-resource "aws_organizations_organizational_unit" "tst" {
-  name = "TEST"
+# ADMIN - OU
+resource "aws_organizations_organizational_unit" "forti-admin" {
+  name = "FORTI-ADMIN"
   parent_id = data.aws_organizations_organization.xperts.roots.0.id
 }
-
-# PROD - OU
-resource "aws_organizations_organizational_unit" "prod" {
-  name      = "PROD"
-  parent_id = data.aws_organizations_organization.xperts.roots.0.id
-}
-
-
