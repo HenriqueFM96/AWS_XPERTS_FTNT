94fad16b5d1b0da0bee0ef0c6008e97e1a24c7b7
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 18:29:33 -0300

    users creation

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "users" {
-  value = ["${aws_iam_user.stdts-accounts.name}"]
+  value = ["${aws_iam_user.stdts-accounts.*.name}"]
 }
 
 output "passwords" {
   sensitive = true
-  value = ["${aws_iam_user_login_profile.stdts_login.password}"]
+  value = ["${aws_iam_user_login_profile.stdts_login.*.password}"]
 }
