8c4fe78ba06aed2e65c292e0ec7390e1862acc4c
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 01:09:17 -0300

    comments added to the source code

--- AWS_iam_groups.tf
+++ AWS_iam_groups.tf
@@ -4,6 +4,7 @@ resource "aws_iam_group" "stdts_all" {
   name = "stdts_all"
 }
 
+# Define users to be part of group name stdts_all 
 resource "aws_iam_user_group_membership" "stdt-membership" {
   user = aws_iam_user.stdt001.name
 
@@ -12,36 +13,43 @@ resource "aws_iam_user_group_membership" "stdt-membership" {
   ]
 }
 
+# Define CFT policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-cft-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.cft_stdt.arn
 }
 
+# Define EC2 policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-ec2-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.ec2_stdt.arn
 }
 
+# Define ECR policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-ecr-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.ecr_stdt.arn
 }
 
+# Define EKS policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-eks-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.eks_stdt.arn
 }
 
+# Define MarketPlace policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-marketplace-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.marketplace_stdt.arn
 }
 
+# Define Route53 policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-route53-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.route53_stdt.arn
 }
 
+# Define S3 policy to be attached on group stdts_all
 resource "aws_iam_group_policy_attachment" "stdts-s3-policies" {
   group      = aws_iam_group.stdts_all.name
   policy_arn = aws_iam_policy.s3_stdt.arn
