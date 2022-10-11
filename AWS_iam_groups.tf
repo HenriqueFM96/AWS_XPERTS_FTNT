d54cb8766c82d3b9d0d70b03b6668e7ef629b430
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 00:44:11 -0300

    users and permissions

---
+++ AWS_iam_groups.tf
@@ -0,0 +1,25 @@
+#TF File to set groups availables in the Organization
+#Students permissions
+resource "aws_iam_group" "stdts_all" {
+  name = "stdts_all"
+}
+
+resource "aws_iam_group_policy_attachment" "stdts-policies" {
+  group      = aws_iam_group.stdts_all.name
+  policy_arn = [
+    "aws:iam::024680062370:policy/cft_stdt",
+    "aws:iam::"
+  ]
+}
+
+## Setting up the admin group account with desired permissions
+resource "aws_iam_group" "xperts-admins" {
+  name = "xperts-admins"
+}
+
+resource "aws_iam_group_policy_attachment" "xperts-policies" {
+  group      = aws_iam_group.xperts-admins.name
+  policy_arn = [
+    "arn:aws:iam::aws:policy/job-function/SystemAdministrator"
+  ]
+}
