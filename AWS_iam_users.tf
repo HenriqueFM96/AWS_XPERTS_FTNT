37728d92bf9e994de505e68da2c44cb56fb30e48
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 01:33:52 -0300

    users' password

--- AWS_iam_users.tf
+++ AWS_iam_users.tf
@@ -2,6 +2,11 @@ resource "aws_iam_user" "stdt001" {
   name = "stdt001"
 }
 
+resource "aws_iam_user_login_profile" "stdt001-profile" {
+    user = aws_iam_user.stdt001.name
+    pgp_key = "keybase:stdt001"
+}
+
 resource "aws_iam_user" "stdt002" {
   name = "stdt002"
 }
