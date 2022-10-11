e1010651f6eece06928b7dd7deee6d4b1dabb321
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 18:23:46 -0300

    users creation

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "users" {
-  value = aws_iam_user.stdts-accounts.name
+  value = aws_iam_user.stdts-accounts[count.index].name
 }
 
 output "passwords" {
   sensitive = true
-  value = var.secret_password
+  value = aws_iam_user_login_profile.stdts_login.password
 }
