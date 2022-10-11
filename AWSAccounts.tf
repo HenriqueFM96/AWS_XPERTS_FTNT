760ffa80fc88838d4bd22e8dfbfd988ad5d4cf71
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 00:16:19 -0300

    OUs creation

--- AWSAccounts.tf
+++ AWSAccounts.tf
@@ -1,5 +1,8 @@
+#AWS Account creation
+
 resource "aws_organizations_account" "account" {
   name      = "my_new_account"
   email     = "hfm@123.com.br"
+  parent_id = "ou-XPERTS-STUDENTS"
   role_name = "OrganizationAccountAccessRole"
 }
