73c58ff5fd2168bfd2c874215f1993b8a582a24c
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 00:48:12 -0300

    users and permissions

--- AWS_iam_users.tf
+++ AWS_iam_users.tf
@@ -1,9 +1,4 @@
 resource "aws_iam_user" "stdt001" {
   name = "stdt001"
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-  groups      = "stdts-all"
+  group = "stdts-all"
 }
