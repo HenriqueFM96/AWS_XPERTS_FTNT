8a00f253a7075221186855d02db9ce8db9ec61e2
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 14:59:34 -0300

    accounts loop

--- AWSAccounts.tf
+++ AWSAccounts.tf
@@ -1,8 +1,25 @@
 #AWS Account creation
 
+resource "aws_organizations_account" "UsersAccount" {
+  count = 3
+  name      = "newStudent${count.index}"
+  email     = "hfm${count.index}@123.com.br"
+  parent_id = "ou-ebvx"
+  role_name = "OrganizationAccountAccessRole"
+}
+
 resource "aws_organizations_account" "account" {
-  name      = "my_new_account"
+  name      = "stdt01"
   email     = "hfm@123.com.br"
   parent_id = "ou-ebvx-vyvxwl6k"
   role_name = "OrganizationAccountAccessRole"
 }
+
+resource "aws_organizations_account" "account" {
+  name      = "stdt02"
+  email     = "student1@fortixperts.com"
+  parent_id = "ou-ebvx-vyvxwl6k"
+  role_name = "OrganizationAccountAccessRole"
+}
+
+#student1@fortixperts.com - student160@fortixperts.com
