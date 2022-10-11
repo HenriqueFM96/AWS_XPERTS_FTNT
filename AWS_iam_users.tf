d5dc168c0a52261f1c38f274a494be9443809a2a
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 16:06:03 -0300

    users creation

--- AWS_iam_users.tf
+++ AWS_iam_users.tf
@@ -1,4 +1,16 @@
-resource "aws_iam_user" "stdtsCreation" {
+resource "aws_iam_user" "stdt001" {
+  name = "stdt001"
+}
+
+resource "aws_iam_user" "stdt002" {
+  name = "stdt002"
+}
+
+resource "aws_iam_user" "stdt003" {
+  name = "stdt003"
+}
+
+resource "aws_iam_user" "stdts-accounts" {
   count = 5
   name = "stdt${count.index}"
 }
