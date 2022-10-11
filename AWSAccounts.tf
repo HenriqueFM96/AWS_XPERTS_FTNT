7a5448bffeef62971049ffee30dbc7af150ad9b8
Henrique Moises <hmoises@fortinet-us.com>
2022-10-11 15:20:17 -0300

    accounts loop

--- AWSAccounts.tf
+++ AWSAccounts.tf
@@ -8,7 +8,7 @@ resource "aws_organizations_account" "UsersAccount" {
 }
 
 resource "aws_organizations_account" "account" {
-  name      = "stdt01"
+  name      = "my_new_account"
   email     = "hfm@123.com.br"
   parent_id = "ou-ebvx-vyvxwl6k"
   role_name = "OrganizationAccountAccessRole"
